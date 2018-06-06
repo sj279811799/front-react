cb6520f8f162f56a675e7d9e6c896b27e801025a
Song <jian.song01@hand-china.com>
2018-06-07 00:22:51 +0800

    [ADD]init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+const el = document.getElementById('app');
+
+render(
+    <App />,
+    el
+);
