24ca60cf304fb6dbf7de4bc0023ebdc993bef8fe
sssok <279811799@qq.com>
2018-07-20 10:57:35 +0800

    [FIX]新增npm包

--- bin/server.js
+++ bin/server.js
@@ -4,17 +4,18 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('../webpack/webpack.dev.config');
 const webpack = require('webpack');
 const path = require('path');
+
 const compiler = webpack(config);
 
 const server = new WebpackDevServer(compiler, {
-    contentBase: path.resolve(__dirname, '../build'), // 默认会以根文件夹提供本地服务器，这里指定文件夹
     inline: true, // 自动刷新
     hot: true, // 开启热模块替换
-    contentBase: path.resolve(__dirname, '../dist'), //默认会以根文件夹提供本地服务器，这里指定文件夹
-    historyApiFallback: true, //在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
-    port: 9001, //如果省略，默认8080
-    publicPath: "/"
+    contentBase: path.resolve(__dirname, '../dist'), // 默认会以根文件夹提供本地服务器，这里指定文件夹
+    historyApiFallback: true, // 在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+    port: 9001, // 如果省略，默认8080
+    publicPath: "/",
 });
+
 server.listen(9001, 'localhost', function (err) {
-    if (err) throw err
-})
+    if (err) throw err;
+});
